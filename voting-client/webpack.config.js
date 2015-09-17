a970efd0214a76bed20f34d466314ce68a2cc47e
James Gibson Weber <j.gibweb@gmail.com>
2015-09-16 22:05:34 -0700

    set up client tooling

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   entry: ['./src/index.js'],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
